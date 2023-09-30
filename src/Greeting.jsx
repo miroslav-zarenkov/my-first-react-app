16495f3b8549397370a9278fad4ca9b0a7c75879
miroslav-zarenkov <miroslav.zarenkov@gmail.com>
2023-09-30 04:21:19 +0400

    init: start task

---
+++ src/Greeting.jsx
@@ -0,0 +1,8 @@
+function Greeting() {
+  return <>
+    <h1>Hello, World!</h1>
+    <div>This is my first React App.</div>
+  </>
+}
+
+export default Greeting;
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import Greeting from './Greeting.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Greeting />
+  </React.StrictMode>,
+)
